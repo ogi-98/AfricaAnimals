3f866274bc1d8080cfaf20db9fc6f5e11bacf618
ogi-98 <oguzkaya99@hotmail.com>
2021-05-23 15:18:01 +0300

    Initial Commit

---
+++ Shared/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Shared
+//
+//  Created by Oğuz Kaya on 23.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
