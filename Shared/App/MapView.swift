f3c576f88a5507279f95bf597ddd874fdea813ff
ogi-98 <oguzkaya99@hotmail.com>
2021-05-25 22:23:23 +0300

    MapView with  basic MapAnnotation

--- Shared/App/MapView.swift
+++ Shared/App/MapView.swift
@@ -6,10 +6,44 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct MapView: View {
+    
+    @State private var region: MKCoordinateRegion = {
+        var mapCoordinates = CLLocationCoordinate2D(latitude: 6.600286, longitude: 16.4377599)
+        var mapZoomLevel = MKCoordinateSpan(latitudeDelta: 70.0, longitudeDelta: 70.0)
+        var mapRegion = MKCoordinateRegion(center: mapCoordinates, span: mapZoomLevel)
+        return mapRegion
+    }()
+    
+    let locations: [NationalParkLocation] = Bundle.main.decode("locations.json")
+    
+    
     var body: some View {
-        Text("MapView")
+//        MARK: - NO.1 BASIC MAP
+//        Map(coordinateRegion: $region)
+//        MARK: - NO.2 ADVENCED MAP
+        Map(coordinateRegion: $region, annotationItems: locations, annotationContent: {
+            item in
+//            (A) old style (always static)
+            
+//            MapPin(coordinate: item.location, tint: .accentColor)
+            
+//            (B) marker: new style (always static)
+            
+//            MapMarker(coordinate: item.location, tint: .accentColor)
+            
+//            (C) Custom basic annotation (it could be interactive)
+            
+            MapAnnotation(coordinate: item.location) {
+                Image("logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 32, height: 32, alignment: .center)
+            }//: ANNOTATİON
+            
+        })//: MAP
     }
 }
 
